27ee590116eae61803356ed27ebf6f8a436572e8
franciscosargenti <franciscosargenti94@gmail.com>
2023-06-01 21:11:47 -0300

    Agrego el archivo conversionGrados.js

---
+++ Practico0/conversionGrados.js
@@ -0,0 +1,4 @@
+let gradosCelsius1 = (50 - 32) * (5/9);
+let gradosCelsius2 = (75 - 32) * (5/9);
+console.log("La conversión de 50 grados Farenheit son "+gradosCelsius1+ " grados Ceclsius");
+console.log("La conversión de 75 grados Farenheit son "+gradosCelsius2+ " grados Ceclsius");
